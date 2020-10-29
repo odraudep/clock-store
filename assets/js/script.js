e05ee04290e5bd806b953054e28e2c642f6ffffa
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-10-28 21:01:50 -0300

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 let clock = document.querySelector('#clock')
 
+function start() {
+  setTimeout(() => {
+    clock.style.transform = "translateY(-10px)"
+    setTimeout(() => {
+    clock.style.transform = "translateY(0)"
+    }, 250)
+  }, 700)
+}
+
+setInterval(() => {
+  setTimeout(() => {
+    clock.style.transform = "translateY(-5px)"
+    setTimeout(() => {
+    clock.style.transform = "translateY(0)"
+    }, 250)
+  }, 0)  
+}, 650) 
+
 function change(x) {
   clock.src = `assets/images/${x}.png`
 }
